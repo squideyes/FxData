b23e9033010d598968613da7006f9e3dde4c64a7
Louis S. Berman <louis@squideyes.com>
2022-05-05 23:15:32 -0400

    Refactored some UnitTests

--- UnitTests/FxData/Models/Extenders/MiscExtendersTests.cs
+++ UnitTests/FxData/Models/Extenders/MiscExtendersTests.cs
@@ -17,6 +17,38 @@ namespace SquidEyes.UnitTests.FxData;
 
 public class MiscExtendersTests
 {
+    [Theory]
+    [InlineData(Source.Dukascopy, "DC")]
+    [InlineData(Source.ForexCom, "FC")]
+    [InlineData(Source.HistData, "HD")]
+    [InlineData(Source.OandaCorp, "OC")]
+    [InlineData(Source.SquidEyes, "SE")]
+    public void ToCodeReturnsExpectedValue(Source source, string expected) =>
+    source.ToCode().Should().Be(expected);
+
+    [Theory]
+    [InlineData("DC", Source.Dukascopy)]
+    [InlineData("FC", Source.ForexCom)]
+    [InlineData("HD", Source.HistData)]
+    [InlineData("OC", Source.OandaCorp)]
+    [InlineData("SE", Source.SquidEyes)]
+    public void ToSourceReturnsExpectedValue(string source, Source expected) =>
+        source.ToSource().Should().Be(expected);
+
+    [Fact]
+    public void ToSourceThrowsErrorOnBadArg()
+    {
+        FluentActions.Invoking(() => "XX".ToSource())
+            .Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [Fact]
+    public void ToCodeThrowsErrorOnBadArg()
+    {
+        FluentActions.Invoking(() => ((Source)0).ToCode())
+            .Should().Throw<ArgumentOutOfRangeException>();
+    }
+
     [Fact]
     public void ToRateWithBadMidOrAskThrowsException()
     {
